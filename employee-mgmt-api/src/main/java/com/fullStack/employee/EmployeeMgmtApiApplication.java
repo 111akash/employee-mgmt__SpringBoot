b169f2e1cd2f1414f91555dbc17a77945c4980d4
akash <akash329@protonmail.com>
2022-10-01 17:29:36 +0530

    added project code

---
+++ employee-mgmt-api/src/main/java/com/fullStack/employee/EmployeeMgmtApiApplication.java
@@ -0,0 +1,14 @@
+package com.fullStack.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgmtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgmtApiApplication.class, args);
+		System.out.println("It seems to be working...");
+	}
+
+}
